5355a069e5de20d8943ba711345ff10f4cb472fd
Lu Thien Tung <tlu21@dxc.com>
2022-04-07 23:25:26 +0700

    reBrandname COCONUT

--- src/components/organisms/Avatar/index.tsx
+++ src/components/organisms/Avatar/index.tsx
@@ -408,7 +408,7 @@ export const Avatar: React.FC<Props> = (props) => {
               <button className="share-button">
                 <TwitterShareButton
                   url={window.location.href}
-                  title="#conteNFT , #NFT"
+                  title="#COCONUT , #NFT"
                   className="Demo__some-network__share-button">
                   <li><Icon className="i-icon" size='big' name='twitter' /></li>
                   <li className="span-icon"><span > Twitter</span></li>
@@ -423,7 +423,7 @@ export const Avatar: React.FC<Props> = (props) => {
               <button className="share-button" >
                 <FacebookShareButton
                   url={window.location.href}
-                  title="#conteNFT , #NFT"
+                  title="#COCONUT , #NFT"
                   className="Demo__some-network__share-button">
                   <li>
                     <Icon className="i-icon" size='big' name='facebook' /></li>
@@ -440,7 +440,7 @@ export const Avatar: React.FC<Props> = (props) => {
               <button className="share-button">
                 <TelegramShareButton
                   url={window.location.href}
-                  title="#conteNFT , #NFT"
+                  title="#COCONUT , #NFT"
                   className="Demo__some-network__share-button">
                   <li>
                     <Icon className="i-icon" size='big' name='telegram plane' /></li>
