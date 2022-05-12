b9075202c0d0fdc53531e76e5749628e64ebfcd8
Lu Thien Tung <tlu21@dxc.com>
2022-05-12 13:58:41 +0700

    set naver search

--- src/components/util/pagemeta/index.tsx
+++ src/components/util/pagemeta/index.tsx
@@ -11,6 +11,12 @@ export const Pagemeta: React.FC<Props> = ({ title }) => {
       <title>{title}</title>
       <link rel="icon"  href={logo} sizes="16x16" />
       <meta name="naver-site-verification" content="fff5aa1d2ca09aa8ce9da616973fc4ef96f97734" ></meta>
+      <meta name="description" content="Coconut Global - The best global short video NFT Marketplace"></meta>
+      <meta property="og:type" content="website"></meta>
+      <meta property="og:title" content="Coconut Global NFT Marketplace"></meta>
+      <meta property="og:description" content="Coconut Global - The best global short video NFT Marketplace"></meta>
+      <meta property="og:image" content="https://app.coconut.global/ccn_logoOF.png"></meta>
+      <meta property="og:url" content="https://app.coconut.global"></meta>
     </Helmet>
   );
 };
