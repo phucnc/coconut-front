b8d1ccf3d9fb21990e2ef5e2e565bc0b378e2815
Lu Thien Tung <tlu21@dxc.com>
2022-05-13 16:50:39 +0700

    meta tag

--- src/components/util/pagemeta/index.tsx
+++ src/components/util/pagemeta/index.tsx
@@ -11,11 +11,11 @@ export const Pagemeta: React.FC<Props> = ({ title }) => {
       <title>{title}</title>
       <link rel="icon"  href={logo} sizes="16x16" />
       <meta name="naver-site-verification" content="fff5aa1d2ca09aa8ce9da616973fc4ef96f97734" ></meta>
-      <meta name="description" content="Coconut Global - The best global short video NFT Marketplace"></meta>
-      <meta name="title" content="Coconut Global NFT Marketplace"></meta>
+      {/* <meta name="description" content="Coconut Global - The best global short video NFT Marketplace"></meta> */}
+      <meta name="title" content="COCONUT.GLOBAL"></meta>
       <meta property="og:type" content="website"></meta>
-      <meta property="og:title" content="Coconut Global NFT Marketplace"></meta>
-      <meta property="og:description" content="Coconut Global - The best global short video NFT Marketplace"></meta>
+      <meta property="og:title" content="COCONUT.GLOBAL"></meta>
+      <meta property="og:description" content="인플루언서들의 NFT 숏폼 서비스 , 숏폼 영상 찍고 거래해 보자"></meta>
       <meta property="og:image" content="https://app.coconut.global/ccn_logoOF.png"></meta>
       <meta property="og:url" content="https://app.coconut.global"></meta>
       <meta name="description" content="인플루언서들의 NFT 숏폼 서비스 , 숏폼 영상 찍고 거래해 보자."></meta>
