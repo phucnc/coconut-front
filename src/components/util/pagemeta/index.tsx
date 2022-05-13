2794867d31abac4d13c4b2bba2dbc840377832f3
Lu Thien Tung <tlu21@dxc.com>
2022-05-13 09:32:50 +0700

    add meta tag title

--- src/components/util/pagemeta/index.tsx
+++ src/components/util/pagemeta/index.tsx
@@ -12,6 +12,7 @@ export const Pagemeta: React.FC<Props> = ({ title }) => {
       <link rel="icon"  href={logo} sizes="16x16" />
       <meta name="naver-site-verification" content="fff5aa1d2ca09aa8ce9da616973fc4ef96f97734" ></meta>
       <meta name="description" content="Coconut Global - The best global short video NFT Marketplace"></meta>
+      <meta name="title" content="Coconut Global NFT Marketplace"></meta>
       <meta property="og:type" content="website"></meta>
       <meta property="og:title" content="Coconut Global NFT Marketplace"></meta>
       <meta property="og:description" content="Coconut Global - The best global short video NFT Marketplace"></meta>
